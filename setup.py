379b5d2a3f4b0bf37b665db1d8d25ea88e409a9d
Ertuğrul <ertugkeremoglu@gmail.com>
2016-02-15 15:44:08 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,18 +11,18 @@ setup(
 
     name = 'Plugon',
     version = '1.0',
-	keywords = 'loop engine',
+    keywords = 'loop engine',
     description = 'Lightweight and simple loop engine.',
-	long_description = 'Plugon is a lightweight and simple loop engine which is made for building efficient and clean architecture for applications such as servers, clients, bots, etc.',
-	license = 'MIT',
+    long_description = 'Plugon is a lightweight and simple loop engine which is made for building efficient and clean architecture for applications such as servers, clients, bots, etc.',
+    license = 'MIT',
 
     author = 'Ertuğrul Keremoğlu',
     author_email = 'ertugkeremoglu@gmail.com',
     url = 'https://github.com/ertugrulkeremoglu/plugon/',
 
     classifiers = [
-		'Development Status :: 3 - Alpha',
-		'Topic :: Utilities',
+        'Development Status :: 3 - Alpha',
+        'Topic :: Utilities',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
@@ -30,7 +30,7 @@ setup(
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX',
         'Programming Language :: Python',
-		'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: MIT License',
     ],
 
     packages = [
